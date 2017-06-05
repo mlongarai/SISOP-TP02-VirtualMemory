5d15018cf477b43c945dbfc0260b17fc6095d3c7
MLongarai <murilo.longarai@gmail.com>
2017-06-05 18:28:52 -0300

    Em desenvolvimento.

--- ATSimul.c
+++ ATSimul.c
@@ -1,6 +1,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
+#include <time.h>
 
 /* TLB with 16 entries. */
 unsigned int TLB[16][2];
@@ -15,10 +17,10 @@ unsigned int PageTable[256];
  * @param pt Page table pointer.
  */
 void page_table_init(unsigned int *pt){
-	int i;
-	for(i=0; i<256; i++){
-		pt[i] = rand()%256;
-	}
+    int i;
+    for(i=0; i<256; i++){
+        pt[i] = rand()%256;
+    }
 }
 
 /**
@@ -27,36 +29,65 @@ void page_table_init(unsigned int *pt){
  * @return VA.
  */
 unsigned int next_address(){
-	return rand()%65536;
+    return rand()%65536;
 }
 
 
 int main(int argc, char **argv){
-	int i;
-	unsigned int address;
-	int numberAddr = 0;
-	
-	if(argc < 2){
-		printf("Usage: ./%s <number of addresses>\n", argv[0]);
-		return 0;
-	}
-	
-	numberAddr = atoi(argv[1]);
+    int i;
+    unsigned int address;
+    int numberAddr = 0;
+    
+    if(argc < 2){
+        printf("Usage: ./%s <number of addresses>\n", argv[0]);
+        return 0;
+    }
+    
+    numberAddr = atoi(argv[1]);
 
-	/* Initialize the random numbers generator. */
-	srand(time());
-	
-	/* Reset TLB */
-	memset(TLB, 0, sizeof(TLB));
-	
-	/* Initial random page table. */
-	page_table_init(PageTable);
-	
-	for(i=0;i<numberAddr;i++){
-		address = next_address();
-		
-		/* WRITE YOUR TRANSLATION CODE HERE!! */
-	}
-	
-	return 0;
+    /* Initialize the random numbers generator. */
+    srand(time());
+    
+    /* Reset TLB */
+    memset(TLB, 0, sizeof(TLB));
+    
+    /* Initial random page table. */
+    page_table_init(PageTable);
+
+    int pagina, offset, answerTLB, enderecoReal,answerPageTable, TLBFault;
+    
+    for(i=0;i<numberAddr;i++){
+        address = next_address();
+        
+        pagina = address >> 8;
+        offset = address & 0XFF;
+
+        answerTLB = buscaTLB(pagina); // procuro no tlb
+                
+        if(answerTLB){ // se achei no tlb
+        
+            enderecoReal = frame << 8 || offset;
+                
+            
+        }
+        else{ // se não achei no tlb
+        TLBFault++; // TLB erro
+    
+        answerPageTable = buscaPageTable(pagina);        
+
+            if(answerPageTable){
+                    enderecoReal = enderecoReal = frame << 8 || offset;
+                    adicionaTLB(pagina,frame); // coloco no TLB
+            
+                } else{
+                    PageTableFault++;
+                    
+                    }
+
+        }
+
+
+    }
+    
+    return 0;
 }
