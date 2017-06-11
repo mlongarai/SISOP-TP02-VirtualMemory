18439fc1ca26318a4a9df356219480e58a1628a4
MLongarai <murilo.longarai@gmail.com>
2017-06-11 16:02:22 -0300

    Em desenvolvimento.

--- ATSimul.c
+++ ATSimul.c
@@ -4,18 +4,12 @@
 #include <unistd.h>
 #include <time.h>
 
-/* TLB with 16 entries. */
-unsigned int TLB[16][2];
+unsigned int TLB[16][2]; /* TLB with 16 entries. */
+unsigned int PageTable[256]; /* Page Table with 256 entries */
 
-/* Page Table with 256 entries */
-unsigned int PageTable[256];
+int frame;
+int posCorrenteTLB = 0;
 
-
-/**
- * @brief Initialize the page table with random frame numbers.
- * 
- * @param pt Page table pointer.
- */
 void page_table_init(unsigned int *pt){
     int i;
     for(i=0; i<256; i++){
@@ -23,15 +17,39 @@ void page_table_init(unsigned int *pt){
     }
 }
 
-/**
- * @brief Generates a random virtual address.
- * 
- * @return VA.
- */
 unsigned int next_address(){
     return rand()%65536;
 }
 
+int buscaTLB(int pagina)
+{
+    int i;
+    int achei = -1;    
+    for(i = 0; i < 16; i++) {
+       
+        if(TLB[i][0] == pagina){
+           frame = TLB[i][1];
+           achei = 1;
+           break;
+         }    
+       }
+return achei;
+}
+
+void preencheTLB(int frame, int pagina) 
+{
+   
+        TLB[posCorrenteTLB][0] = pagina;
+        TLB[posCorrenteTLB][1] = frame;
+        posCorrenteTLB = (posCorrenteTLB + 1) % 16;
+}
+
+
+int buscaPageTable(int pagina)
+{
+    frame = PageTable[pagina];
+    preencheTLB(frame,pagina);
+}
 
 int main(int argc, char **argv){
     int i;
@@ -39,55 +57,50 @@ int main(int argc, char **argv){
     int numberAddr = 0;
     
     if(argc < 2){
+
         printf("Usage: ./%s <number of addresses>\n", argv[0]);
         return 0;
+    
     }
     
     numberAddr = atoi(argv[1]);
 
-    /* Initialize the random numbers generator. */
-    srand(time());
+    srand(time()); /* Initialize the random numbers generator. */
+    memset(TLB, 0, sizeof(TLB)); /* Reset TLB */
+    page_table_init(PageTable); /* Initial random page table. */
+    int pagina, offset, answerTLB, enderecoReal,answerPageTable, TLBFault;    
     
-    /* Reset TLB */
-    memset(TLB, 0, sizeof(TLB));
-    
-    /* Initial random page table. */
-    page_table_init(PageTable);
-
-    int pagina, offset, answerTLB, enderecoReal,answerPageTable, TLBFault;
-    
-    for(i=0;i<numberAddr;i++){
-        address = next_address();
+    for(i=0;i<numberAddr;i++){ 
         
+        address = next_address();
+
         pagina = address >> 8;
+        
         offset = address & 0XFF;
 
-        answerTLB = buscaTLB(pagina); // procuro no tlb
+        answerTLB = buscaTLB(pagina); 
                 
-        if(answerTLB){ // se achei no tlb
-        
+        if(answerTLB == 0){ 
+            
             enderecoReal = frame << 8 || offset;
-                
-            
+        
+        } /* match tlb anwser */
+        else{ /* tlb no anwser */
+        
+        TLBFault++;
+        
         }
-        else{ // se não achei no tlb
-        TLBFault++; // TLB erro
-    
-        answerPageTable = buscaPageTable(pagina);        
+        
+        buscaPageTable(pagina);        
 
-            if(answerPageTable){
-                    enderecoReal = enderecoReal = frame << 8 || offset;
-                    adicionaTLB(pagina,frame); // coloco no TLB
+        enderecoReal = enderecoReal = frame << 8 || offset;
             
-                } else{
-                    PageTableFault++;
-                    
-                    }
-
+            printf("Endereço Virtual: %x\n", address);
+            printf("\nEndereço Real: %x\n", enderecoReal);
+            printf("TLB Fault:%d\n", TLBFault);                
+        
         }
 
-
-    }
-    
     return 0;
+
 }
--- ATSimul2.c
+++ ATSimul2.c
@@ -1,115 +1,106 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
+#include <time.h>
 
-/* TLB with 16 entries. */
-int TLB[16][2];
+unsigned int TLB[16][2]; /* TLB with 16 entries. */
+unsigned int PageTable[256]; /* Page Table with 256 entries */
 
-/* Page Table with 256 entries */
-unsigned int PageTable[256];
+int frame;
+int posCorrenteTLB = 0;
 
-
-/**
- * @brief Initialize the page table with random frame numbers.
- * 
- * @param pt Page table pointer.
- */
 void page_table_init(unsigned int *pt){
-	int i;
-	for(i=0; i<256; i++){
-		pt[i] = rand()%256;
-	}
+    int i;
+    for(i=0; i<256; i++){
+        pt[i] = rand()%256;
+    }
 }
 
-/**
- * @brief Generates a random virtual address.
- * 
- * @return VA.
- */
 unsigned int next_address(){
-	return rand()%65536;
+    return rand()%65536;
 }
 
-unsigned int buscaTLB(int pagina){
-    
-    for(int i =0; i <= 256; i++)
-    {
-      if(TLB[i][0] == pagina)
-        return 1;    
-    }    
-    return 1;
+int buscaTLB(int pagina)
+{
+    int i;
+    int achei = -1;    
+    for(i = 0; i < 16; i++) {
+       
+        if(TLB[i][0] == pagina){
+           frame = TLB[i][1];
+           achei = 1;
+           break;
+         }    
+       }
+return achei;
 }
 
-unsigned int buscaPageTable(int pagina){
-    
-    for(int i =0; i <= 16; i++)
-    {
-      if(PageTable[i] == pagina)
-        return 1;    
-    }    
-    return 1;
+void preencheTLB(int frame, int pagina) 
+{
+   
+        TLB[posCorrenteTLB][0] = pagina;
+        TLB[posCorrenteTLB][1] = frame;
+        posCorrenteTLB = (posCorrenteTLB + 1) % 16;
 }
 
-void adicionaTLB(pagina,frame){
 
-    
+int buscaPageTable(int pagina)
+{
+    frame = PageTable[pagina];
+    preencheTLB(frame,pagina);
 }
-
 
 int main(int argc, char **argv){
-	int i, pagina, offset, answerTLB, enderecoReal,answerPageTable, TLBFault,frame,PageTableFault;
-	unsigned int address;
-	int numberAddr = 0;
-	
-	if(argc < 2){
-		printf("Usage: ./%s <number of addresses>\n", argv[0]);
-		return 0;
-	}
-	
-	numberAddr = atoi(argv[1]);
-
-	/* Initialize the random numbers generator. */
-	srand(time());
-	
-	/* Reset TLB */
-	memset(TLB, -1, sizeof(TLB));
-	
-	/* Initial random page table. */
-	page_table_init(PageTable);
-	
-    for(i=0;i<numberAddr;i++){
-        address = next_address();
-        
-        pagina = address >> 8;
-        offset = address & 0XFF;
-
-        answerTLB = buscaTLB(pagina); // procuro no tlb
-                
-        if(answerTLB){ // se achei no tlb
-        
-            enderecoReal = frame << 8 || offset;
-                
-            
-        }
-        else{ // se não achei no tlb
-        TLBFault++; // TLB erro
+    int i;
+    unsigned int address;
+    int numberAddr = 0;
     
-        answerPageTable = buscaPageTable(pagina);        
+    if(argc < 2){
 
-            if(answerPageTable){
-                    enderecoReal = enderecoReal = frame << 8 || offset;
-                    adicionaTLB(pagina,frame); // coloco no TLB
-            
-                } else{
-                    PageTableFault++;
-                    
-                    }
-
-        }
-
-
+        printf("Usage: ./%s <number of addresses>\n", argv[0]);
+        return 0;
+    
     }
     
-	
-	return 0;
+    numberAddr = atoi(argv[1]);
+
+    srand(time()); /* Initialize the random numbers generator. */
+    memset(TLB, 0, sizeof(TLB)); /* Reset TLB */
+    page_table_init(PageTable); /* Initial random page table. */
+    int pagina, offset, answerTLB, enderecoReal,answerPageTable, TLBFault;    
+    
+    for(i=0;i<numberAddr;i++){ 
+        
+        address = next_address();
+
+        pagina = address >> 8;
+        
+        offset = address & 0XFF;
+
+        answerTLB = buscaTLB(pagina); 
+                
+        if(answerTLB == 0){ 
+            
+            enderecoReal = frame << 8 || offset;
+        
+        } /* match tlb anwser */
+        else{ /* tlb no anwser */
+        
+        TLBFault++;
+        
+        }
+        
+        buscaPageTable(pagina);        
+
+        enderecoReal = enderecoReal = frame << 8 || offset;
+            
+            printf("Endereço Virtual: %x\n", address);
+            printf("\nEndereço Real: %x\n", enderecoReal);
+            printf("TLB Fault:%d\n", TLBFault);                
+        
+        }
+
+    return 0;
+
 }
